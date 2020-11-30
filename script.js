371ab304aaf3b5c1c1ed77c53c6a02cca80acfea
Marketing <marketing@Marketings-iMac.local>
2020-11-30 12:21:54 -0800

    Added new section

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+// This is the quote Modal
+
+$(".hero-btn").on("click", function() {
+    var heroBtn = $(".hero-btn");
+    var quoteModal = $(".quote-modal");
+
+
+    if (heroBtn == heroBtn) {
+        quoteModal.show();
+        quoteModal.css("display", "flex");
+
+    } else {
+        console.log("something Went Wrong");
+    }
+})
+
+$(".exitIcon").on("click", function() {
+    var quoteModal = $(".quote-modal");
+    var exitIcon = $(".exitIcon");
+
+
+    if (exitIcon == exitIcon) {
+        quoteModal.hide();
+
+    } else {
+        console.log("something Went Wrong");
+    }
+})
+
+
 $(".main-logo-outside").on("click", function() {
     $(".section-indicator").toggle("slide", { direction: "right" }, 500);
     $(".indicator-logo-outside").toggle("slide", { direction: "right" }, 500);
@@ -43,3 +73,39 @@ setInterval(function() {
         $(".hiw-4-hover").css("display", "none");
     }
 }, 200);
+
+
+// This is the learn 5 minutes Section 
+// poster frame click event
+$(document).on('click', '.js-videoPoster', function(ev) {
+    ev.preventDefault();
+    var $poster = $(this);
+    var $wrapper = $poster.closest('.js-videoWrapper');
+    videoPlay($wrapper);
+});
+
+// play the targeted video (and hide the poster frame)
+function videoPlay($wrapper) {
+    var $iframe = $wrapper.find('.js-videoIframe');
+    var src = $iframe.data('src');
+    // hide poster
+    $wrapper.addClass('videoWrapperActive');
+    // add iframe src in, starting the video
+    $iframe.attr('src', src);
+}
+
+// stop the targeted/all videos (and re-instate the poster frames)
+function videoStop($wrapper) {
+    // if we're stopping all videos on page
+    if (!$wrapper) {
+        var $wrapper = $('.js-videoWrapper');
+        var $iframe = $('.js-videoIframe');
+        // if we're stopping a particular video
+    } else {
+        var $iframe = $wrapper.find('.js-videoIframe');
+    }
+    // reveal poster
+    $wrapper.removeClass('videoWrapperActive');
+    // remove youtube link, stopping the video from playing in the background
+    $iframe.attr('src', '');
+}
